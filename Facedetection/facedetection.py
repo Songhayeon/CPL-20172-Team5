53dc068c70ea7665160efc76e77166910100871c
신동길\User <nidor02@hotmail.com>
2017-09-24 07:25:50 +0900

    source for detecting face.
     it detect face and save face image.

---
+++ Facedetection/facedetection.py
@@ -0,0 +1,51 @@
+import cv2
+import numpy as np
+
+# 라이브러리 등록
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+
+# 폰트 등록
+
+def faceDetect():
+    face_cascade = cv2.CascadeClassifier('C:\\Program Files\\OpenCV3.3\\opencv\\build\\etc\\haarcascades\\haarcascade_frontalface_default.xml')
+    upper_cascade = cv2.CascadeClassifier('C:\\Program Files\\OpenCV3.3\\opencv\\build\\etc\\haarcascades\\haarcascade_upperbody.xml')
+    # 영상에서 인식할 소스 등록
+    try:
+        cap = cv2.VideoCapture(0)
+        print('카메라 로딩 성공')
+    # 웹캠 활성화시키는 코드
+
+    except:
+        print('카메라 로딩 실패')
+        return
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            return
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 2, 0, (30, 30))
+        upper = upper_cascade.detectMultiScale(gray, 1.3, 2, 0, (30,30))
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 3, 4, 0)
+            cv2.putText(frame, 'Detected Face', (x - 5, y - 5), font, 0.9, (255, 255, 0), 2)
+            img_trim = frame[y:y+ h, x:x+w]
+            cv2.imwrite('SSong.jpg', img_trim)
+        # 얼굴을 인식하는 사각형에 대한 소스, 텍스트 소스
+
+        cv2.imshow('frame', frame)
+        # 영상을 출력하는 소스
+
+        if cv2.waitKey(1) == 255:
+            break;
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+# space를 누르면 실행 종료되는 코드
+
+faceDetect()
